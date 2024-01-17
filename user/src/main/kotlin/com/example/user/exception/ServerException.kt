8401f71b258ebb4677ee834868b2dee4da1cbf63
cherhy <ekxk1234@naver.com>
2024-01-17 23:54:23 +0900

    add invalid jwt token exception

--- user/src/main/kotlin/com/example/user/exception/ServerException.kt
+++ user/src/main/kotlin/com/example/user/exception/ServerException.kt
@@ -5,14 +5,18 @@ sealed class ServerException(
     val code: Int,
 ): RuntimeException(message)
 
-class UserExistException(
+data class UserExistException(
     override val message: String = "이미 존재하는 이메일입니다.",
 ): ServerException(message, 409)
 
-class UserNotFoundException(
+data class UserNotFoundException(
     override val message: String = "회원이 존재하지 않습니다.",
 ): ServerException(message, 404)
 
-class PasswordNotMatchException(
+data class PasswordNotMatchException(
     override val message: String = "비밀번호가 일치하지 않습니다.",
+): ServerException(message, 400)
+
+data class InvalidJwtTokenException(
+    override val message: String = "잘못된 토큰입니다.",
 ): ServerException(message, 400)

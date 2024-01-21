5fa236e13a002096e458df31140473dc1c59f0fa
lolmageap <ekxk1234@naver.com>
2024-01-21 17:13:20 +0900

    add upload file logic

--- user/src/main/kotlin/com/example/user/domain/UserController.kt
+++ user/src/main/kotlin/com/example/user/domain/UserController.kt
@@ -1,6 +1,11 @@
 package com.example.user.domain
 
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.MediaType
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.*
+import java.io.File
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -31,5 +36,38 @@ class UserController(
         @AuthToken token: String,
     ): MeResponse =
         userService.getByToken(token)
+            .let(MeResponse::invoke)
+
+    @GetMapping("/{userId}/username")
+    suspend fun getUsername(
+        @PathVariable userId: Long,
+    ): Map<String, String> =
+        mapOf("username" to userService.get(userId).username)
+
+    @PostMapping("/{id}", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    suspend fun edit(
+        @PathVariable id: Long,
+        @ModelAttribute request: UserEditRequest,
+        @AuthToken token: String,
+        @RequestPart("profileUrl") filePart: FilePart,
+    ) {
+        val originalFileName = filePart.filename()
+
+        val file = if (originalFileName.isNotBlank()) {
+            val etc = originalFileName.substring(
+                originalFileName.lastIndexOf(".") + 1
+            )
+            val newFileName = "$id.$etc"
+            val file = File(ClassPathResource("images").file, newFileName)
+            filePart.transferTo(file).awaitSingle()
+            newFileName
+        } else {
+            null
+        }
+
+        userService.edit(token, request, file)
+
+    }
 
 }
+///Users/cherhy/IdeaProjects/kotlin-issue-service/user/src/main/resources/images
